007f7bacf83734380ddb44a42d3e456d942d4ca7
Anton <antonrozdobudko@gmail.com>
2021-07-31 21:26:00 +0300

    HomeWork #1

---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import c from './Message.module.css'
+
+type messageType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+function Message(props: messageType) {
+    return (
+        <div className={c.container}>
+            <div className={c.forImage}>
+                <img src={props.avatar} alt="avatar"/>
+            </div>
+            <div className={c.blackbox}>
+                <div>
+                    <div><span className={c.name}>{props.name}</span></div>
+                    <div><span className={c.message}>{props.message}</span></div>
+                </div>
+                <div>
+                    <span className={c.time}>{props.time}</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Message
