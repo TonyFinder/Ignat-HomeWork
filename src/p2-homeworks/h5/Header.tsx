71e3e670254dfff6f8f700df9f00367576965074
Anton <antonrozdobudko@gmail.com>
2021-10-25 10:36:27 +1000

    HomeWork #5

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import {PATH} from './Routes';
 
 function Header() {
     return (
         <div>
-            // add NavLinks
-
+            {/*add NavLinks*/}
+            <NavLink to={PATH.PRE_JUNIOR}>Pre-Junior</NavLink>
+            <NavLink to={PATH.JUNIOR}> Junior</NavLink>
+            <NavLink to={PATH.JUNIOR_PLUS}> Junior-Plus</NavLink>
         </div>
     )
 }
