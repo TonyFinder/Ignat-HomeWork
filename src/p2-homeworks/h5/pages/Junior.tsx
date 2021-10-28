0288deb88af53cd0aafd3c934e0ca227a66790e3
Anton <antonrozdobudko@gmail.com>
2021-10-28 14:34:30 +1000

    HomeWork #9. Timer + CSS.

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -2,9 +2,10 @@ import React, {useState} from 'react'
 import s from './Junior.module.css';
 import HW7 from '../../h7/HW7';
 import HW8 from '../../h8/HW8';
+import HW9 from '../../h9/HW9';
 
 function Junior() {
-    const [collapse, setCollapse] = useState<Array<boolean>>([false, false])
+    const [collapse, setCollapse] = useState<Array<boolean>>([false, false, false])
     const setHandler = (hw: number) => {
         const newCollapse = [...collapse]
         newCollapse[hw - 1] = !newCollapse[hw - 1]
@@ -17,6 +18,8 @@ function Junior() {
             {collapse[0] && <HW7/>}
             <button className={s.tasks} onClick={() => setHandler(2)}>Homeworks #8</button>
             {collapse[1] && <HW8/>}
+            <button className={s.tasks} onClick={() => setHandler(3)}>Homeworks #9</button>
+            {collapse[2] && <HW9/>}
         </div>
     )
 }
--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,50 +1,53 @@
 import React, {useState} from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import s from './Clock.module.css'
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
+    const [date, setDate] = useState<Date>(new Date())
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
-        // stop
+        clearInterval(timerId)
     }
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
-            // setDate
+            setDate(new Date())
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
-        // show
+        setShow(true)
     }
     const onMouseLeave = () => {
-        // close
+        setShow(false)
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    const stringTime = date.toLocaleTimeString('ru-RU') // fix with date
+    const stringDate = date.toLocaleDateString('ru-RU')
 
     return (
-        <div>
-            <div
-                onMouseEnter={onMouseEnter}
-                onMouseLeave={onMouseLeave}
-            >
-                {stringTime}
+        <div className={s.main}>
+            <div className={s.buttons}>
+                <SuperButton onClick={start}>start</SuperButton>
+                <SuperButton onClick={stop}>stop</SuperButton>
             </div>
-
-            {show && (
-                <div>
-                    {stringDate}
+            <div className={s.dateTime}>
+                <div
+                    onMouseEnter={onMouseEnter}
+                    onMouseLeave={onMouseLeave}
+                >
+                    {stringTime}
                 </div>
-            )}
 
-            <SuperButton onClick={start}>start</SuperButton>
-            <SuperButton onClick={stop}>stop</SuperButton>
-
+                {show && (
+                    <div>
+                        {stringDate}
+                    </div>
+                )}
+            </div>
         </div>
     )
 }
